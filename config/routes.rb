f14dc19e8498bdf218bc932302c19c2bbd43c809
xapyka <hrk528_n2@yahoo.co.jp>
2016-03-09 12:34:54 +0000

    Add account activations

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   post 'login'    => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
   resources :users
+  resources :account_activations, only: [:edit]
   # get 'static_pages/signup'
   # get 'static_pages/login'
   # The priority is based upon order of creation: first created -> highest priority.
